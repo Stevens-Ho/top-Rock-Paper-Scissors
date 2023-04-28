d3b507d883957454ae17643af218d4ebbea85242
loveprogrammming <106758997+loveprogrammming@users.noreply.github.com>
2023-04-29 00:17:42 +0800

    Update function playRound()

--- javascript.js
+++ javascript.js
@@ -17,22 +17,28 @@ function playRound(playerSelection, computerSelection) {
     //lose
     if (playerSelection === "rock" && computerSelection === "paper") {
         console.log("You Lose! Paper beats Rock");
+        computerWin++;
     }
     else if (playerSelection === "paper" && computerSelection === "scissor") {
         console.log("You Lose! Scissor beats Paper");  
+        computerWin++;
     }
     else if (playerSelection === "scissor" && computerSelection === "rock") {
         console.log("You Lose! Rock beats Scissor");
+        computerWin++;
     }
     //win
     else if (playerSelection === "paper" && computerSelection === "rock") {
         console.log("You Win! Paper beats Rock");
+        userWin++;
     }
     else if (playerSelection === "rock" && computerSelection === "scissor") {
         console.log("You Win! Rock beats Scissor");
+        userWin++;
     }
     else if (playerSelection === "scissor" && computerSelection === "paper") {
         console.log("You Win! Scissor beats Paper");
+        userWin++;
     }
     //tie
     else if (playerSelection === "scissor" && computerSelection === "scissor") {
