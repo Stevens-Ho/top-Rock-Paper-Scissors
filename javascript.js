339add3262c1673c977c0c91a52c5376be607a05
loveprogrammming <106758997+loveprogrammming@users.noreply.github.com>
2023-04-28 23:56:36 +0800

    Add function playRound()

--- javascript.js
+++ javascript.js
@@ -13,3 +13,35 @@ function getComputerChoice() {
 
 let computerWin = 0;
 let userWin = 0;
+function playRound(playerSelection, computerSelection) {
+    //lose
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log("You Lose! Paper beats Rock");
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissor") {
+        console.log("You Lose! Scissor beats Paper");  
+    }
+    else if (playerSelection === "scissor" && computerSelection === "rock") {
+        console.log("You Lose! Rock beats Scissor");
+    }
+    //win
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("You Win! Paper beats Rock");
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissor") {
+        console.log("You Win! Rock beats Scissor");
+    }
+    else if (playerSelection === "scissor" && computerSelection === "paper") {
+        console.log("You Win! Scissor beats Paper");
+    }
+    //tie
+    else if (playerSelection === "scissor" && computerSelection === "scissor") {
+        console.log("It's a tie");
+    }
+    else if (playerSelection === "paper" && computerSelection === "paper") {
+        console.log("It's a tie"); 
+    }
+    else if (playerSelection === "rock" && computerSelection === "rock") {
+        console.log("It's a tie"); 
+    }
+}
