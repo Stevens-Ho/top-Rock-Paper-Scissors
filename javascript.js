b57edfe90bd9cdf2a85ac73cb959cf4fc6239f07
loveprogrammming <106758997+loveprogrammming@users.noreply.github.com>
2023-04-28 23:54:40 +0800

    Add function getComputerChoice()

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let number = Math.floor(Math.random()*3);
+    if (number === 0) {
+        return "rock";
+    }
+    else if (number === 1) {
+        return "paper";
+    }
+    else if (number === 2) {
+        return "scissor";
+    }
+}
