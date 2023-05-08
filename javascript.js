12631067010bc9fcc1e5fd5ef9d7ab0708aef4bb
loveprogrammming <106758997+loveprogrammming@users.noreply.github.com>
2023-05-08 22:41:21 +0800

    Display the result in the browser

--- javascript.js
+++ javascript.js
@@ -11,44 +11,51 @@ function getComputerChoice() {
     }
 }
 
+function displayResult(resultContent) {
+    let div = document.querySelector("div");
+    let result = document.querySelector(".result");
+    result.textContent = resultContent;
+}
+
 let computerWin = 0;
 let userWin = 0;
 function playRound(playerSelection, computerSelection) {
     //lose
     if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log("You Lose! Paper beats Rock");
+        document.create
+        displayResult("You Lose! Paper beats Rock");
         computerWin++;
     }
     else if (playerSelection === "paper" && computerSelection === "scissor") {
-        console.log("You Lose! Scissor beats Paper");  
+        displayResult("You Lose! Scissor beats Paper");  
         computerWin++;
     }
     else if (playerSelection === "scissor" && computerSelection === "rock") {
-        console.log("You Lose! Rock beats Scissor");
+        displayResult("You Lose! Rock beats Scissor");
         computerWin++;
     }
     //win
     else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You Win! Paper beats Rock");
+        displayResult("You Win! Paper beats Rock");
         userWin++;
     }
     else if (playerSelection === "rock" && computerSelection === "scissor") {
-        console.log("You Win! Rock beats Scissor");
+        displayResult("You Win! Rock beats Scissor");
         userWin++;
     }
     else if (playerSelection === "scissor" && computerSelection === "paper") {
-        console.log("You Win! Scissor beats Paper");
+        displayResult("You Win! Scissor beats Paper");
         userWin++;
     }
     //tie
     else if (playerSelection === "scissor" && computerSelection === "scissor") {
-        console.log("It's a tie");
+        displayResult("It's a tie");
     }
     else if (playerSelection === "paper" && computerSelection === "paper") {
-        console.log("It's a tie"); 
+        displayResult("It's a tie"); 
     }
     else if (playerSelection === "rock" && computerSelection === "rock") {
-        console.log("It's a tie"); 
+        displayResult("It's a tie"); 
     }
 }
 
